00c03d413eb6d4e6c14d373d86b30f6b8a28f841
Igor Sicorskiy <idriveview@gmail.com>
2026-02-14 22:19:50 +0300

    255

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!rrrrrrrrrrrrrrrrrrrr25");
+Console.WriteLine("Hello, World!rrrrrrrrrrrrrrrrrrrr2555");
