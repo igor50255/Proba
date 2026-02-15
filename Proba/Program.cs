4192f53754673605891013d1e2fcf66ac9ab5236
igor50255 <iviewcloud@gmail.com>
2026-02-15 11:41:57 +0300

    Update Program.cs

--- Proba/Program.cs
+++ Proba/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!rrrrrrrrrrrrrrrrrrrr2555");
+Console.WriteLine("Hello, World!rrrrrrrrrrrrrrrrrrrr25551111");
